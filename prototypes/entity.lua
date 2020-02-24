6fe7fae7b1df00a7b1695c1b9156822c5d0de528
Pengwin83 <david.schepers@udo.edu>
2020-02-24 20:23:17 +0100

    ~ Changed formatting

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -1,77 +1,77 @@
 --entity.lua
 if data.raw["simple-entity-with-force"]["se-core-miner"] and data.raw["mining-drill"]["se-core-miner-drill"] then
 
-	local shadow =
-	{
-	  filename = "__space-exploration-graphics__/graphics/entity/core-miner/hr/core-miner-shadow.png",
-	  priority = "high",
-	  width = 951,
-	  height = 491,
-	  frame_count = 1,
-	  shift = {(2 + 3/32) *3/11, (1 + 22/32) *3/11},
-	  draw_as_shadow = true,
-	  scale=0.5 *3/11,
-	}
-	local shadow_anim = table.deepcopy(shadow)
-	shadow_anim.repeat_count = 30
-	local off_layer = {
-	  layers = {
-		shadow,
-		{
-		  filename = "__space-exploration-graphics__/graphics/entity/core-miner/hr/core-miner-off.png",
-		  priority = "high",
-		  width = 691,
-		  height = 737,
-		  frame_count = 1,
-		  shift = {0, -8/32 *3/11},
-		  scale=0.5 *3/11,
-		},
-	  }
-	}
+    local shadow =
+    {
+      filename = "__space-exploration-graphics__/graphics/entity/core-miner/hr/core-miner-shadow.png",
+      priority = "high",
+      width = 951,
+      height = 491,
+      frame_count = 1,
+      shift = {(2 + 3/32) *3/11, (1 + 22/32) *3/11},
+      draw_as_shadow = true,
+      scale=0.5 *3/11,
+    }
+    local shadow_anim = table.deepcopy(shadow)
+    shadow_anim.repeat_count = 30
+    local off_layer = {
+      layers = {
+        shadow,
+        {
+          filename = "__space-exploration-graphics__/graphics/entity/core-miner/hr/core-miner-off.png",
+          priority = "high",
+          width = 691,
+          height = 737,
+          frame_count = 1,
+          shift = {0, -8/32 *3/11},
+          scale=0.5 *3/11,
+        },
+      }
+    }
 
-	local placeholder = table.deepcopy(data.raw["simple-entity-with-force"]["se-core-miner"])
-	placeholder.name = "asteroid-miner-placeholder"
-	placeholder.minable = {mining_time = 0.5, result = "asteroid-miner-placeholder"}
-	placeholder.max_health = 300
-	placeholder.corpse = "medium-remnants"
-	placeholder.dying_explosion = "medium-explosion"
-	placeholder.collision_box = {{-1.2, -1.2}, {1.2, 1.2}}
+    local placeholder = table.deepcopy(data.raw["simple-entity-with-force"]["se-core-miner"])
+    placeholder.name = "asteroid-miner-placeholder"
+    placeholder.minable = {mining_time = 0.5, result = "asteroid-miner-placeholder"}
+    placeholder.max_health = 300
+    placeholder.corpse = "medium-remnants"
+    placeholder.dying_explosion = "medium-explosion"
+    placeholder.collision_box = {{-1.2, -1.2}, {1.2, 1.2}}
     placeholder.collision_mask = {
       "item-layer",
       "object-layer",
       "player-layer",
       "water-tile"
     }
-	placeholder.selection_box = {{-1.5, -1.5}, {1.5, 1.5}}
+    placeholder.selection_box = {{-1.5, -1.5}, {1.5, 1.5}}
     placeholder.pictures = {
       off_layer,
       off_layer,
       off_layer,
       off_layer
     }
-	
-	
-	local asteroidMiner = table.deepcopy(data.raw["mining-drill"]["se-core-miner-drill"])
-	asteroidMiner.name = "asteroid-miner-drill"
-	asteroidMiner.minable = {mining_time = 0.5, result = "asteroid-miner-placeholder"}
+
+
+    local asteroidMiner = table.deepcopy(data.raw["mining-drill"]["se-core-miner-drill"])
+    asteroidMiner.name = "asteroid-miner-drill"
+    asteroidMiner.minable = {mining_time = 0.5, result = "asteroid-miner-placeholder"}
     selection_priority = 10
     icon = "__space-exploration-graphics__/graphics/icons/core-miner.png"
     icon_size = 32
     order = "zzz"
-	flags = {"placeable-neutral", "placeable-player", "player-creation", "not-blueprintable", "not-deconstructable"}
-	asteroidMiner.max_health = 300	
+    flags = {"placeable-neutral", "placeable-player", "player-creation", "not-blueprintable", "not-deconstructable"}
+    asteroidMiner.max_health = 300	
     corpse = "medium-remnants"
     dying_explosion = "medium-explosion"
     resistances = {{type = "fire", percent = 70 }}
-	asteroidMiner.collision_box = {{-1.2, -1.2}, {1.2, 1.2}}
+    asteroidMiner.collision_box = {{-1.2, -1.2}, {1.2, 1.2}}
     asteroidMiner.selection_box = {{-1.5, -1.5}, {1.5, 1.5}}
-	asteroidMiner.resource_categories = { "asteroid-mining" }
-	resource_searching_radius = 2
-	asteroidMiner.mining_speed = 1
-	always_draw_idle_animation = false
-	asteroidMiner.energy_usage = "100kW"
-	asteroidMiner.vector_to_place_result = { 0, -1.85 }
-	asteroidMiner.animations = {
+    asteroidMiner.resource_categories = { "asteroid-mining" }
+    resource_searching_radius = 2
+    asteroidMiner.mining_speed = 1
+    always_draw_idle_animation = false
+    asteroidMiner.energy_usage = "100kW"
+    asteroidMiner.vector_to_place_result = { 0, -1.85 }
+    asteroidMiner.animations = {
       layers = {
         shadow_anim,
         {
@@ -129,7 +129,7 @@ if data.raw["simple-entity-with-force"]["se-core-miner"] and data.raw["mining-dr
       }
     }
     asteroidMiner.energy_source =
-	{
+    {
       type = "electric",
       usage_priority = "secondary-input",
       emissions_per_minute = 10,
@@ -140,6 +140,6 @@ if data.raw["simple-entity-with-force"]["se-core-miner"] and data.raw["mining-dr
     }
     asteroidMiner.allowed_effects = {"consumption", "speed", "productivity", "pollution"}
 
-	data:extend{placeholder}
-	data:extend{asteroidMiner}
+    data:extend{placeholder}
+    data:extend{asteroidMiner}
 end
--- prototypes/resources.lua
+++ prototypes/resources.lua
@@ -11,43 +11,43 @@ local resource_image = {
 }
 
 local asteroid_resource_patch = {
-	type = "resource",
-	name = "asteroid-dust-resource",
-	category = "asteroid-mining",
-	collision_box = { { -1.4, -1.4  }, { 1.4, 1.4 } },
-	flags = { "placeable-neutral" },
-	highlight = false,
-	icon_size = 32,
-	icons = 
-	{
-		{
-			icon = "__space-exploration-bridge-mod__/graphics/icons/asteroid_dust.png",
-			icon_size = 32
-		}
-	},
-	infinite = true,
-	infinite_depletion_amount = 1,
-	map_color = { b = 0, g = 0, r = 0, a = 0 },
-	map_grid = false,
-	minable = {
-		mining_time = 1,
-		results = {
-			{
-				amount_max = 1,
-				amount_min = 1,
-				name = "asteroid-dust",
-				probability = 1,
-			}
-		}
-	},
-	minimum = 800,
-	normal = 1000,
-	order = "a-b-a",
-	resource_patch_search_radius = 0,
-	selection_box = {{ -0.0, -0.0 },{ 0.0, 0.0 }},
-	stage_counts = { 0 },
-	stages = { sheet = resource_image },
-	localised_name = "asteroid-dust-source"
+    type = "resource",
+    name = "asteroid-dust-resource",
+    category = "asteroid-mining",
+    collision_box = { { -1.4, -1.4  }, { 1.4, 1.4 } },
+    flags = { "placeable-neutral" },
+    highlight = false,
+    icon_size = 32,
+    icons = 
+    {
+        {
+            icon = "__space-exploration-bridge-mod__/graphics/icons/asteroid_dust.png",
+            icon_size = 32
+        }
+    },
+    infinite = true,
+    infinite_depletion_amount = 1,
+    map_color = { b = 0, g = 0, r = 0, a = 0 },
+    map_grid = false,
+    minable = {
+        mining_time = 1,
+        results = {
+            {
+                amount_max = 1,
+                amount_min = 1,
+                name = "asteroid-dust",
+                probability = 1,
+            }
+        }
+    },
+    minimum = 800,
+    normal = 1000,
+    order = "a-b-a",
+    resource_patch_search_radius = 0,
+    selection_box = {{ -0.0, -0.0 },{ 0.0, 0.0 }},
+    stage_counts = { 0 },
+    stages = { sheet = resource_image },
+    localised_name = "asteroid-dust-source"
 }
 
 data:extend({asteroid_resource_patch})
